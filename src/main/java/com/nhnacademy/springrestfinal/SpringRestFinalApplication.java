14e4225ffc4266371a252dcba9f4f8edf1a485cc
조승주 <tmdwn3318@gmail.com>
2024-11-21 11:18:49 +0900

    init

---
+++ src/main/java/com/nhnacademy/springrestfinal/SpringRestFinalApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springrestfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/springrestfinal/SpringRestFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springrestfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
